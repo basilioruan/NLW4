279fdef5b96e1a8d79c8d98bcd065a9043dea098
basilioruan <rmb@estudante.ufla.br>
2021-02-27 15:55:33 -0300

    Aula-3: Separação do repository, refatoração do server e dos controllers e criação dos testes de integração

--- NodeJS/API/src/routes.ts
+++ NodeJS/API/src/routes.ts
@@ -1,10 +1,14 @@
 import {Router} from 'express';
 import { UserController } from './controllers/UserController';
+import { SurveysController } from './controllers/SurveysController';
 
 const router = Router();
 
 const usersController = new UserController();
+const surveysController = new SurveysController();
 
 router.post("/users", usersController.create);
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export {router};
